2a4d74c5e422c4c15cdf65771ca73445c561b03d
Galen Rice <griceturrble@protonmail.com>
2023-12-05 09:27:03 -0500

    feat: 2023 day 3 complete

--- 2023/aoc2023/day03/README.md
+++ 2023/aoc2023/day03/README.md
@@ -4,15 +4,14 @@ Link: <https://adventofcode.com/2023/day/3>
 
 ## Part 1
 
-We need to identify numbers in the schematic, which is the first issue.
-For that we should probably identify them via regex and find a means to parse out thier positions
-(either the start pos or the bounding box).
-If we know a number's position, presumably we can form a box around that number and get all symbols in that box.
-If any symbols are present other than `.`, then the part is a part number and should be pulled.
+We need to identify numbers in the schematic, which is the first issue. For that we should probably identify them via regex and find a means to parse out thier positions (either the start pos or the bounding box). If we know a number's position, presumably we can form a box around that number and get all symbols in that box. If any symbols are present other than `.`, then the part is a part number and should be pulled.
 
-So, we take substrings that contain and are adjacent to a number
-(row above and below, one space to left and right included)
-and test whether those substrings contain a symbol other than a digit or `.`.
-When one is found, we total up the numbers and find our part 1 solution.
+So, we take substrings that contain and are adjacent to a number (row above and below, one space to left and right included) and test whether those substrings contain a symbol other than a digit or `.`. When one is found, we total up the numbers and find our part 1 solution.
 
 ## Part 2
+
+Target numbers are only those near a `*` character, so we can skip most of the processing the looks for numbers in the first place. In fact, going from the numbers themselves could be problematic, since we would need to jump from a number to a `*` and then daisychain to another number.
+
+Thinking about it as I write, actually that wouldn't be terrible, but we can't do it in one pass. We'd go from finding a number on a line to finding a nearby `*`, then record the position of that asterisk as a point in a HashMap. We could then map that point to a `Vec<i32>`, onto which we can push the number we just found.
+
+After that pass, it's a simple matter of identifying points that have attached Vectors of exactly two numbers (applying a filter). With those combinations of numbers in hand, we obtain their products, sum the total, and there's our answer.
--- 2023/aoc2023/day03/src/main.rs
+++ 2023/aoc2023/day03/src/main.rs
@@ -3,6 +3,7 @@
 use lube::{get_file_contents, get_input_file_path};
 use regex::Regex;
 use std::cmp;
+use std::collections::HashMap;
 use std::ops::Range;
 use substring::Substring;
 
@@ -21,16 +22,33 @@ fn is_a_part_num(rows: &[String], colrange: &Range<usize>) -> bool {
     false
 }
 
+fn find_gear_point(
+    _contents: &Vec<String>,
+    rowrange: Range<usize>,
+    colrange: Range<usize>,
+) -> Option<(usize, usize)> {
+    let re: Regex = Regex::new(r"\*").expect("Failed to parse partnum regex");
+    for row in rowrange {
+        let start_index: usize = cmp::max((colrange.start as i32) - 1, 0) as usize;
+        let end_index: usize = cmp::min(colrange.end + 1, _contents[row].len() - 1);
+        let substr: &str = _contents[row].substring(start_index, end_index);
+        if let Some(mat) = re.find(&substr) {
+            return Option::Some((row, mat.range().start + start_index));
+        }
+    }
+    None
+}
+
 /// Part 1 solution
 fn part_one(_contents: &Vec<String>) {
     let mut total: i32 = 0;
     let re: Regex = Regex::new(r"\d+").expect("Failed to parse numbers pattern");
     for (line_num, line) in _contents.iter().enumerate() {
-        let content_rows_range: Range<usize> = Range {
+        let rowrange: Range<usize> = Range {
             start: cmp::max((line_num as i32) - 1, 0) as usize,
             end: cmp::min((line_num as i32) + 2, (_contents.len() as i32) - 1) as usize,
         };
-        let content_rows: &[String] = &_contents[content_rows_range];
+        let content_rows: &[String] = &_contents[rowrange];
         let matches: Vec<(i32, Range<usize>)> = re
             .find_iter(line)
             .map(|m| {
@@ -41,8 +59,8 @@ fn part_one(_contents: &Vec<String>) {
             })
             .collect();
         for mat in matches {
-            let (num, range) = mat;
-            if is_a_part_num(&content_rows, &range) {
+            let (num, colrange) = mat;
+            if is_a_part_num(&content_rows, &colrange) {
                 total += num;
             }
         }
@@ -52,7 +70,45 @@ fn part_one(_contents: &Vec<String>) {
 
 /// Part 2 solution
 fn part_two(_contents: &Vec<String>) {
-    println!("...world!");
+    // our running total for the solution
+    let mut total: i32 = 0;
+    // hash map of points where the gears are located.
+    let mut gear_points: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+    // regex to find numbers within the string
+    let re: Regex = Regex::new(r"\d+").expect("Failed to parse numbers pattern");
+    for (line_num, line) in _contents.iter().enumerate() {
+        let rowrange: Range<usize> = Range {
+            start: cmp::max((line_num as i32) - 1, 0) as usize,
+            end: cmp::min((line_num as i32) + 2, (_contents.len() as i32) - 1) as usize,
+        };
+        // let content_rows: &[String] = &_contents[rowrange];
+        let matches: Vec<(i32, Range<usize>)> = re
+            .find_iter(line)
+            .map(|m| {
+                (
+                    m.as_str().parse().expect("Could not parse number"),
+                    m.range(),
+                )
+            })
+            .collect();
+        for mat in matches {
+            let (num, colrange) = mat;
+            if let Some(point) = find_gear_point(_contents, rowrange.clone(), colrange.clone()) {
+                gear_points
+                    .entry(point)
+                    .and_modify(|v| v.push(num))
+                    .or_insert(vec![num]);
+            }
+        }
+    }
+
+    for (_, nums) in gear_points {
+        if nums.len() == 2 {
+            total += nums[0] * nums[1];
+        }
+    }
+    // println!("{:?}", gear_points);
+    println!(">> {total}");
 }
 
 fn main() {
