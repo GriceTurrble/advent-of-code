e9e322db196e7414da09bbcb49a9c89ebd9da831
Galen Rice <griceturrble@protonmail.com>
2023-12-06 10:47:09 -0500

    feat: 2023 day 6 complete

--- 2023/aoc2023/day06/src/part1.rs
+++ 2023/aoc2023/day06/src/part1.rs
@@ -1,6 +1,38 @@
 /// Part 2 solution
 pub fn solution(contents: &Vec<String>) {
     println!("-------------------- PART 1 --------------------");
-    println!("{:?}", contents[0]);
-    println!("Hello...");
+    let mut cum_total: u32 = 1;
+    let times: Vec<u32> = contents[0]
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()[1..]
+        .iter()
+        .map(|s| s.parse().expect("Failed to parse numbers for times"))
+        .collect();
+    let distances: Vec<u32> = contents[1]
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()[1..]
+        .iter()
+        .map(|s| s.parse().expect("Failed to parse numbers for times"))
+        .collect();
+    println!("Times: {:?}", times);
+    println!("Distances: {:?}", distances);
+
+    for i in 0..times.len() {
+        let mut wins: u32 = 0;
+        for run in 1..times[i] {
+            // time minus length of the current run is the amount of time we push the button,
+            // which is our speed. It's also how much we subtract from the time
+            // to multiple that remaining time
+            let speed = times[i] - run;
+            let remaining = times[i] - speed;
+            let distance = speed * remaining;
+            if distance > distances[i] {
+                wins += 1;
+            }
+        }
+        cum_total *= wins;
+    }
+    println!(">> {cum_total} product of ways to win");
 }
--- 2023/aoc2023/day06/src/part2.rs
+++ 2023/aoc2023/day06/src/part2.rs
@@ -1,6 +1,25 @@
 /// Part 2 solution
+#[allow(unused_mut)]
 pub fn solution(contents: &Vec<String>) {
-    println!("-------------------- PART 2 --------------------");
-    println!("{:?}", contents[0]);
-    println!("Hello...");
+    println!("-------------------- PART 1 --------------------");
+    let (_, timing) = contents[0]
+        .split_once(':')
+        .expect("Failed to split the time string");
+    let timing: u64 = timing.replace(" ", "").parse().expect("Failed to parse number");
+    let (_, distance) = contents[1]
+        .split_once(':')
+        .expect("Failed to split the time string");
+    let distance: u64 = distance.replace(" ", "").parse().expect("Failed to parse number");
+
+    let mut wins: u32 = 0;
+    for speed in 1..timing {
+        let remaining = timing - speed;
+        let remaining_dist = speed * remaining;
+        if remaining_dist > distance {
+            wins += 1;
+        }
+    }
+    println!("Timing: {:?}", timing);
+    println!("Distance: {:?}", distance);
+    println!(">> {wins} ways to win");
 }
