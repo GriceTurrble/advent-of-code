c24a1f494b90bca4c8ffce35e57bb371cdf3cb5c
Galen Rice <griceturrble@protonmail.com>
2023-12-06 11:01:32 -0500

    chore: 2023 day 6 part 2 reformat
    Someone got a pre-commit hook for that?

--- 2023/aoc2023/day06/src/part2.rs
+++ 2023/aoc2023/day06/src/part2.rs
@@ -5,11 +5,17 @@ pub fn solution(contents: &Vec<String>) {
     let (_, timing) = contents[0]
         .split_once(':')
         .expect("Failed to split the time string");
-    let timing: u64 = timing.replace(" ", "").parse().expect("Failed to parse number");
+    let timing: u64 = timing
+        .replace(" ", "")
+        .parse()
+        .expect("Failed to parse number");
     let (_, distance) = contents[1]
         .split_once(':')
         .expect("Failed to split the time string");
-    let distance: u64 = distance.replace(" ", "").parse().expect("Failed to parse number");
+    let distance: u64 = distance
+        .replace(" ", "")
+        .parse()
+        .expect("Failed to parse number");
 
     let mut wins: u32 = 0;
     for speed in 1..timing {
