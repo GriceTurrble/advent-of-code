3a7f9a164738b202fe035c40e7b3f4a48cb7abcf
Galen Rice <griceturrble@protonmail.com>
2023-12-07 15:43:25 -0500

    chore: some kata work added to the lube package
    Save some stuff here for easier reference in future.
    Also use as a test ground (sic)
    for writing proper tests.

--- 2023/aoc2023/lube/src/lib.rs
+++ 2023/aoc2023/lube/src/lib.rs
@@ -18,7 +18,8 @@ pub fn get_input_file_path() -> std::path::PathBuf {
 /// Given a file path for input, reads text from that file and produces a Vector of Strings,
 /// one for each line of the file.
 pub fn get_file_contents(file_path: std::path::PathBuf) -> String {
-    let contents: String = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents: String =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
     contents
 }
 
@@ -28,13 +29,35 @@ pub fn reverse_str(input: &str) -> String {
     reversed
 }
 
+pub fn find_shortest_word(s: &str) -> u32 {
+    s.split_whitespace().map(|w| w.len()).min().unwrap_or(0) as u32
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn reverse_string_works() {
+    fn test_reverse_string() {
         let result = reverse_str(&"abcdefg");
         assert_eq!(result, "gfedcba");
+    }
+
+    #[test]
+    fn test_find_shortest_word() {
+        let stuff: Vec<(&str, u32)> = vec![
+            ("bitcoin take over the world maybe who knows perhaps", 3),
+            (
+                "turns out random test cases are easier than writing out basic ones",
+                3,
+            ),
+            ("lets talk about javascript the best language", 3),
+            ("i want to travel the world writing code one day", 1),
+            ("Lets all go on holiday somewhere very cold", 2),
+            ("Let's travel abroad shall we", 2),
+        ];
+        for (teststr, expected) in stuff {
+            assert_eq!(find_shortest_word(teststr), expected);
+        }
     }
 }
