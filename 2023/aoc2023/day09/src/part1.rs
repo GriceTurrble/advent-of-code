978d6f6e0125962cd49655001d7dbefa17528e84
Galen Rice <griceturrble@protonmail.com>
2023-12-09 09:25:02 -0500

    feat: 2023 day 9 part 1 complete

--- 2023/aoc2023/day09/src/part1.rs
+++ 2023/aoc2023/day09/src/part1.rs
@@ -1,23 +1,41 @@
 /// Part 1 solution
-pub fn solution(contents: &Vec<&str>) -> &'static str {
-    println!("Part 1 incomplete!");
+pub fn solution(contents: &Vec<&str>) -> i32 {
     println!("{:?}", contents[0]);
-    "XYZ"
+    let mut total: i32 = 0;
+    for line in contents {
+        let nums: Vec<i32> = line.split_whitespace().map(|c| c.parse().unwrap()).collect();
+        total += get_next_num(nums);
+    }
+    total
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn get_next_num(nums: Vec<i32>) -> i32 {
+    let num_zeros = nums.iter().filter(|&&x| x != 0).count();
+    if num_zeros == 0 {
+        0
+    } else {
+        // Build the next set of diffs for the input line and recurse
+        let mut next_set: Vec<i32> = Vec::new();
+        for i in 0..(nums.len()-1) {
+            next_set.push(nums[i+1] - nums[i]);
+        }
+        nums[nums.len()-1] + get_next_num(next_set)
+    }
+}
 
-//     fn get_sample_data() -> Vec<&'static str> {
-//         let data = "";
-//         data.trim().split("\n").collect()
-//     }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_solution() {
-//         let result = solution(&get_sample_data());
-//         let expected = "XYZ";
-//         assert_eq!(result, expected);
-//     }
-// }
+    fn get_sample_data() -> Vec<&'static str> {
+        let data = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45";
+        data.trim().split("\n").collect()
+    }
+
+    #[test]
+    fn test_solution() {
+        let result = solution(&get_sample_data());
+        let expected = 114;
+        assert_eq!(result, expected);
+    }
+}
