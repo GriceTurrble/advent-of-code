e1d2f51c620b4e6482a4a6f34b0dc1eaa267be3f
Galen Rice <griceturrble@protonmail.com>
2024-12-01 15:38:15 -0500

    feat: cli options for generating new days
    Day 2 scaffolded from the generator this way.

---
+++ 2024/python/src/grice_py_aoc_2024/cli.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import toml
+
+import click
+
+DIR = Path(__file__).parent
+MAIN_PY_TEMPLATE = DIR / "main.py-tpl"
+TEST_PY_TEMPLATE = DIR / "test_day.py-tpl"
+
+
+@click.group()
+def cli():
+    pass
+
+
+def _add_day_to_pyproject_toml(day: int) -> None:
+    pyproject_toml = DIR / "../../pyproject.toml"
+    if not pyproject_toml.exists():
+        raise click.ClickException(f"pyproject.toml not found at {pyproject_toml}")
+
+    content = toml.loads(pyproject_toml.read_text())
+    day_key = f"day{day:0>2}"
+    scripts = content["project"]["scripts"]
+    if day_key in scripts:
+        raise click.ClickException(f"Day {day} script already exists in pyproject.toml")
+    scripts[day_key] = f"grice_py_aoc_2024.{day_key}.main:main"
+
+    pyproject_toml.write_text(toml.dumps(content))
+
+
+def _create_day_files(day: int) -> None:
+    day_path = DIR / f"day{day:0>2}"
+    if day_path.exists():
+        raise click.ClickException(f"Day {day} already exists at {day_path}")
+
+    day_path.mkdir()
+    (day_path / "__init__.py").touch()
+    (day_path / "inputs.txt").touch()
+    (day_path / "test_inputs.txt").touch()
+    _create_templates(day=day, path=day_path)
+
+
+def _create_templates(day: int, path: Path) -> None:
+    # Create the file for the main code from template
+    main_py = path / "main.py"
+    main_py.touch()
+    main_py.write_text(MAIN_PY_TEMPLATE.read_text())
+
+    # Create the file for the test code from template
+    test_py = path / f"test_day{day:0>2}.py"
+    test_py.touch()
+    test_py.write_text(TEST_PY_TEMPLATE.read_text())
+
+
+@cli.command()
+@click.argument("day", type=int)
+def create_day(day: int) -> None:
+    day_path = DIR / f"day{day:0>2}"
+    if day_path.exists():
+        raise click.ClickException(f"Day {day} already exists at {day_path}")
+    _create_day_files(day=day)
+    _add_day_to_pyproject_toml(day=day)
+
+    print(f">> Day {day} generated at:\n    {day_path}/main.py")
+    print(f">> You can run the day's code using `uv run day{day:0>2}`")
+    print("   and run tests for this and any other day using `uv run pytest`")
+
+
+if __name__ == "__main__":
+    cli()
---
+++ 2024/python/src/grice_py_aoc_2024/day02/test_day02.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from .main import part1, part2
+
+TEST_FILE = Path(__file__).parent / "test_inputs.txt"
+
+RESULT_PART_1 = 123
+RESULT_PART_2 = 456
+
+def test_part1():
+    with open(TEST_FILE) as f:
+        result = part1(f)
+    assert result == RESULT_PART_1
+
+
+def test_part2():
+    with open(TEST_FILE) as f:
+        result = part2(f)
+    assert result == RESULT_PART_2
