a56a5eb0c6cffdb27be6c96a4defd022e553f53f
Galen Rice <griceturrble@protonmail.com>
2024-12-03 05:13:20 -0500

    feaet: py 2024 day 03 complete

--- 2024/python/src/grice_py_aoc_2024/day03/main.py
+++ 2024/python/src/grice_py_aoc_2024/day03/main.py
@@ -1,33 +1,47 @@
 from __future__ import annotations
 
+import re
 import time
-import typing
 from pathlib import Path
-
-if typing.TYPE_CHECKING:
-    from io import TextIOWrapper
 
 FILE = Path(__file__).parent / "inputs.txt"
 
 
-def part1(inputs: TextIOWrapper):
-    return "Not done yet!"
+def part1(contents: str) -> int:
+    mul_pat = re.compile(r"mul\(\d+,\d+\)")
+    content = mul_pat.findall(contents)
+    result = 0
+    for bit in content:
+        left, right = map(int, bit[4:-1].split(","))
+        result += left * right
+    return result
 
 
-def part2(inputs: TextIOWrapper):
-    return "Not done yet!"
+def part2(contents: str) -> int:
+    mul_pat = re.compile(r"do\(\)|don't\(\)|mul\(\d+,\d+\)")
+    content = mul_pat.findall(contents)
+    result = 0
+    enabled = True
+    for bit in content:
+        if bit in ("do()", "don't()"):
+            enabled = bit == "do()"
+            continue
+        if enabled:
+            left, right = map(int, bit[4:-1].split(","))
+            result += left * right
+    return result
 
 
 def main():
+    contents = FILE.read_text()
+
     _start1 = time.perf_counter()
-    with open(FILE) as f:
-        result1 = part1(f)
+    result1 = part1(contents)
     _delta1 = time.perf_counter() - _start1
     print(f">> Part 1: {result1} ({_delta1:.6f}s)")
 
     _start2 = time.perf_counter()
-    with open(FILE) as f:
-        result2 = part2(f)
+    result2 = part2(contents)
     _delta2 = time.perf_counter() - _start2
     print(f">> Part 2: {result2} ({_delta2:.6f}s)")
 
