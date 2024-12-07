fb59d1b5fba03a4bdb36c3b0d119c203847404ca
Galen Rice <griceturrble@protonmail.com>
2024-12-07 04:29:41 -0500

    chore: other fixes on test templates

--- 2024/python/src/grice_py_aoc_2024/day06/test_day06.py
+++ 2024/python/src/grice_py_aoc_2024/day06/test_day06.py
@@ -8,18 +8,17 @@ from .main import part1, part2
 
 DIR = Path(__file__).parent
 TEST_FILE = DIR.parents[3] / "inputs/tests" / f"test_{DIR.stem}.txt"
-EXPECTED_PART_1 = "Now it's done!"
-EXPECTED_PART_2 = "Now it's done!"
+EXPECTED_PART_1 = "REPLACE ME!"
+EXPECTED_PART_2 = "REPLACE ME!"
 
 
-@pytest.mark.skip("Skipped day 6")
 def test_part1():
     contents = TEST_FILE.read_text()
     result = part1(contents)
     assert result == EXPECTED_PART_1
 
 
-@pytest.mark.skip("Skipped day 6")
+@pytest.mark.xfail(reason=f"{DIR.stem} P1 incomplete")
 def test_part2():
     contents = TEST_FILE.read_text()
     result = part2(contents)
--- 2024/python/src/grice_py_aoc_2024/templates/test_day.py-tpl
+++ 2024/python/src/grice_py_aoc_2024/templates/test_day.py-tpl
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import pytest
+
 from .main import part1, part2
 
 DIR = Path(__file__).parent
 TEST_FILE = DIR.parents[3] / "inputs/tests" / f"test_{DIR.stem}.txt"
-EXPECTED_PART_1 = "Now it's done!"
-EXPECTED_PART_2 = "Now it's done!"
+EXPECTED_PART_1 = "REPLACE ME!"
+EXPECTED_PART_2 = "REPLACE ME!"
 
 
 def test_part1():
@@ -16,6 +18,7 @@ def test_part1():
     assert result == EXPECTED_PART_1
 
 
+@pytest.mark.xfail(reason=f"{DIR.stem} P1 incomplete")
 def test_part2():
     contents = TEST_FILE.read_text()
     result = part2(contents)
