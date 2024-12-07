607cc57731bcfc835eb3bba13efb7b15f2feb6ed
Galen Rice <griceturrble@protonmail.com>
2024-12-07 04:11:43 -0500

    feat: 2024 day 8 scaffolded

---
+++ 2024/python/src/grice_py_aoc_2024/day08/main.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import time
+from pathlib import Path
+
+DIR = Path(__file__).parent
+FILE = DIR.parents[3] / "inputs" / f"{DIR.stem}.txt"
+
+
+def part1(contents: str):
+    return "Not done yet!"
+
+
+def part2(contents: str):
+    return "Not done yet!"
+
+
+def main():
+    contents = FILE.read_text()
+
+    _start1 = time.perf_counter()
+    result1 = part1(contents)
+    _delta1 = time.perf_counter() - _start1
+    print(f">> Part 1: {result1} ({_delta1:.6f}s)")
+
+    _start2 = time.perf_counter()
+    result2 = part2(contents)
+    _delta2 = time.perf_counter() - _start2
+    print(f">> Part 2: {result2} ({_delta2:.6f}s)")
+
+
+if __name__ == "__main__":
+    main()
