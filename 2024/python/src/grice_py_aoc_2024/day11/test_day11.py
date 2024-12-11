d6f776a39105ebce896429cac783d6fee2de7b7b
Galen Rice <griceturrble@protonmail.com>
2024-12-11 02:14:59 -0500

    chore: day 11 tests fixed

--- 2024/python/src/grice_py_aoc_2024/day11/test_day11.py
+++ 2024/python/src/grice_py_aoc_2024/day11/test_day11.py
@@ -9,17 +9,18 @@ from .main import part1, part2
 DIR = Path(__file__).parent
 TEST_FILE = DIR.parents[3] / "inputs/tests" / f"test_{DIR.stem}.txt"
 EXPECTED_PART_1 = 55312
-EXPECTED_PART_2 = "REPLACE ME!"
+# The problem actually does not report this number.
+# After solving it naturally, this result came back in tests.
+# So, we roll with it!
+EXPECTED_PART_2 = 65601038650482
 
 
-@pytest.mark.skip(reason=f"{DIR.stem} P1 incomplete")
 def test_part1():
     contents = list(map(int, TEST_FILE.read_text().strip().split()))
     result = part1(contents)
     assert result == EXPECTED_PART_1
 
 
-@pytest.mark.xfail(reason=f"{DIR.stem} P1 incomplete")
 def test_part2():
     contents = list(map(int, TEST_FILE.read_text().strip().split()))
     result = part2(contents)
