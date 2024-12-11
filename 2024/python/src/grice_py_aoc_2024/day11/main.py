0cc29200b6e0d4930b1b8abea2546be099fc1e05
Galen Rice <griceturrble@protonmail.com>
2024-12-11 02:13:18 -0500

    feat: day 11 complete

--- 2024/python/src/grice_py_aoc_2024/day11/main.py
+++ 2024/python/src/grice_py_aoc_2024/day11/main.py
@@ -1,22 +1,43 @@
 from __future__ import annotations
 
 import time
+from functools import cache
 from pathlib import Path
 
 DIR = Path(__file__).parent
 FILE = DIR.parents[3] / "inputs" / f"{DIR.stem}.txt"
 
 
-def part1(contents: str):
-    return "Not done yet!"
+@cache
+def blink(num: int, iters: int) -> int:
+    # Borrowed some insight from folks in PyDis.
+    # We don't need to store most of the real numbers, just the number of stones that
+    # result from iterating on a particular stone N times.
+    # Combined with caching results of stone X iterated N times,
+    # we get a very quick response in both parts.
+    if iters == 0:
+        return 1
+    if num == 0:
+        return blink(1, iters - 1)
+    if len(numstr := str(num)) % 2 == 0:
+        half = len(numstr) // 2
+        return blink(int(numstr[half:]), iters - 1) + blink(
+            int(numstr[:half]),
+            iters - 1,
+        )
+    return blink(num * 2024, iters - 1)
 
 
-def part2(contents: str):
-    return "Not done yet!"
+def part1(contents: list[int]) -> int:
+    return sum(blink(x, 25) for x in contents)
+
+
+def part2(contents: list[int]) -> int:
+    return sum(blink(x, 75) for x in contents)
 
 
 def main():
-    contents = FILE.read_text()
+    contents = list(map(int, FILE.read_text().strip().split()))
 
     _start1 = time.perf_counter()
     result1 = part1(contents)
--- 2024/python/src/grice_py_aoc_2024/day11/test_day11.py
+++ 2024/python/src/grice_py_aoc_2024/day11/test_day11.py
@@ -8,19 +8,19 @@ from .main import part1, part2
 
 DIR = Path(__file__).parent
 TEST_FILE = DIR.parents[3] / "inputs/tests" / f"test_{DIR.stem}.txt"
-EXPECTED_PART_1 = "REPLACE ME!"
+EXPECTED_PART_1 = 55312
 EXPECTED_PART_2 = "REPLACE ME!"
 
 
 @pytest.mark.skip(reason=f"{DIR.stem} P1 incomplete")
 def test_part1():
-    contents = TEST_FILE.read_text()
+    contents = list(map(int, TEST_FILE.read_text().strip().split()))
     result = part1(contents)
     assert result == EXPECTED_PART_1
 
 
 @pytest.mark.xfail(reason=f"{DIR.stem} P1 incomplete")
 def test_part2():
-    contents = TEST_FILE.read_text()
+    contents = list(map(int, TEST_FILE.read_text().strip().split()))
     result = part2(contents)
     assert result == EXPECTED_PART_2
