fec56a870d8536f264aae4d6b53e8d7d68ab3b24
Galen Rice <griceturrble@protonmail.com>
2024-12-14 03:47:04 -0500

    feat: day 12 partial (all parts incomplete)

--- 2024/python/src/grice_py_aoc_2024/day12/main.py
+++ 2024/python/src/grice_py_aoc_2024/day12/main.py
@@ -7,9 +7,41 @@ DIR = Path(__file__).parent
 FILE = DIR.parents[3] / "inputs" / f"{DIR.stem}.txt"
 
 
+def _calc_fence_perimeter(grid: list[str], x: int, y: int, char: str) -> int:
+    len_x, len_y = len(grid), len(grid[0])
+    fences = 0
+    for dx, dy in [
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, 0),
+    ]:
+        newx, newy = x + dx, y + dy
+        if not 0 <= newx < len_x or not 0 <= newy < len_y:
+            # Position is outside the grid: this side naturally has a fence.
+            fences += 1
+        elif char != grid[newx][newy]:
+            # The adjacent region is not the same as our current one,
+            # so a dividing fence is needed.
+            fences += 1
+        # Natural 'else' is an adjacent region with the same type of flower,
+        # so no fence is needed there.
+    return fences
+
+
+MAX_FENCE = 4
+
+
 def part1(grid: list[str]) -> int:
     total = 0
-    # TODO part 1
+    for x, line in enumerate(grid):
+        for y, char in enumerate(line):
+            fences_here = _calc_fence_perimeter(grid=grid, x=x, y=y, char=char)
+            if fences_here == MAX_FENCE:
+                # This is a self-contained region: no further calculation needed.
+                # Area is 1, with 4 fences
+                total += fences_here
+                continue
     return total
 
 
