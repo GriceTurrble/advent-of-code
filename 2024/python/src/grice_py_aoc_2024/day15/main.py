283f18560f7cdef12ab99249cafd326b1749b677
Galen Rice <griceturrble@protonmail.com>
2024-12-15 12:11:20 -0500

    chore: rich added to py deps; day 15 scaffolded

---
+++ 2024/python/src/grice_py_aoc_2024/day15/main.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import time
+from pathlib import Path
+
+DIR = Path(__file__).parent
+FILE = DIR.parents[3] / "inputs" / f"{DIR.stem}.txt"
+
+
+def part1(contents: str) -> int:
+    total = 0
+    # TODO part 1
+    return total
+
+
+def part2(contents: str) -> int:
+    total = 0
+    # TODO part 2
+    return total
+
+
+def main():
+    contents = FILE.read_text()
+
+    _start1 = time.perf_counter()
+    result1 = part1(contents=contents)
+    _delta1 = time.perf_counter() - _start1
+    print(f">> Part 1: {result1} ({_delta1:.6f}s)")
+
+    _start2 = time.perf_counter()
+    result2 = part2(contents=contents)
+    _delta2 = time.perf_counter() - _start2
+    print(f">> Part 2: {result2} ({_delta2:.6f}s)")
+
+
+if __name__ == "__main__":
+    main()
