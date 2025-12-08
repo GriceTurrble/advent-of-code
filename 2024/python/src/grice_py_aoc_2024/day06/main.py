530549ed570a81bd80e86d01aae1556026472d05
Galen Rice <griceturrble@protonmail.com>
2025-12-08 15:10:30 -0500

    fix: remove unused import

--- 2024/python/src/grice_py_aoc_2024/day06/main.py
+++ 2024/python/src/grice_py_aoc_2024/day06/main.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import time
 from collections import deque
 from pathlib import Path
-from typing import TypeVar
 
 DIR = Path(__file__).parent
 FILE = DIR.parents[3] / "inputs" / f"{DIR.stem}.txt"
